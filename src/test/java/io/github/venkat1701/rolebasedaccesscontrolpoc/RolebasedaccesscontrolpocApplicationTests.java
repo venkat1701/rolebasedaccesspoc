1dece58d1f2d7030923e6873c3fe0f1815bb7287
venkat1701 <thejeastdev@gmail.com>
2024-10-20 23:54:02 +0530

    authentication-authorization

---
+++ src/test/java/io/github/venkat1701/rolebasedaccesscontrolpoc/RolebasedaccesscontrolpocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.venkat1701.rolebasedaccesscontrolpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolebasedaccesscontrolpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
